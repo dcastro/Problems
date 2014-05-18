cfb5dfcb4794ae2c8c342e617afa914fbe3935bc
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-05-18 21:02:30 +0100

    Added Sieve of Eratosthenes

---
+++ src/Algorithms/SieveOfEratosthenes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class SieveOfEratosthenes
+    {
+        public static IEnumerable<int> PrimesWithin(int range)
+        {
+            if (range < 2)
+                throw new ArgumentOutOfRangeException("range", "range must be equal to or higher than 2.");
+
+            // [0, range)
+            bool[] marks = Enumerable.Range(0, range)
+                                     .Select(e => true)
+                                     .ToArray();
+
+            for (int i = 2; i < Math.Sqrt(range); i++)
+                if (marks[i])
+                    for (int j = i*i; j < range; j += i)
+                        marks[j] = false;
+
+            return marks.Select((isPrime, index) => new {isPrime, index})
+                        .Skip(2) //skip 0 and 1
+                        .Where(pair => pair.isPrime)
+                        .Select(pair => pair.index);
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/SieveOfEratosthenesTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class SieveOfEratosthenesTests
+    {
+        [Fact]
+        public void PrimesWithin_Returns_PrimeNumbersWithinRange()
+        {
+            int[] expectedPrimes =
+                {
+                    2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                    31, 37, 41, 43, 47, 53, 59, 61, 67,
+                    71, 73, 79, 83, 89, 97, 101
+                };
+            var primes = SieveOfEratosthenes.PrimesWithin(102);
+
+            Assert.Equal(expectedPrimes, primes);
+        }
+    }
+}
