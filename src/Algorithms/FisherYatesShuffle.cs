e53886b2840454309ee7a1ddc4fba3d6e1e37925
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-05-18 20:03:07 +0100

    Added Fisher-Yates Shuffle algorithm

---
+++ src/Algorithms/FisherYatesShuffle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class FisherYatesShuffle
+    {
+        private static readonly Lazy<Random> LazyRnd = new Lazy<Random>(() => new Random());
+ 
+        private static Random Rnd
+        {
+            get { return LazyRnd.Value; }
+        }
+
+        public static void ShuffleInPlace<T>(T[] array, Random rnd = null)
+        {
+            rnd = rnd ?? Rnd;
+
+            Action<T[], int, int> swap = (arr, i, j) =>
+                {
+                    var temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                };
+
+            for (int i = array.Length - 1; i >= 1; i--)
+            {
+                int j = rnd.Next(0, i + 1);
+                swap(array, i, j);
+            }
+        }
+
+        public static T[] Shuffle<T>(T[] array, Random rnd = null)
+        {
+            rnd = rnd ?? Rnd;
+            var shuffled = new T[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int j = rnd.Next(0, i + 1);
+                if (j != i)
+                    shuffled[i] = shuffled[j];
+                shuffled[j] = array[i];
+            }
+
+            return shuffled;
+        }
+
+        public static IEnumerable<T> Shuffle<T>(IEnumerable<T> source, Random rnd = null)
+        {
+            rnd = rnd ?? Rnd;
+            var shuffled = new List<T>();
+
+            foreach (var elem in source)
+            {
+                int j = rnd.Next(0, shuffled.Count + 1);
+                if (j == shuffled.Count)
+                {
+                    shuffled.Add(elem);
+                }
+                else
+                {
+                    shuffled.Add(shuffled[j]);
+                    shuffled[j] = elem;
+                }
+            }
+
+            return shuffled;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/FisherYatesShuffleTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class FisherYatesShuffleTests
+    {
+        [Fact]
+        public void ShuffleInPlace_ShufflesElements()
+        {
+            var rnd = new Random(1);
+            int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int[] expected = {7, 9, 5, 2, 10, 8, 6, 4, 1, 3};
+            FisherYatesShuffle.ShuffleInPlace(arr, rnd);
+
+            Assert.Equal(expected, arr);
+        }
+
+        [Fact]
+        public void Shuffle_ShufflesElements()
+        {
+            var rnd = new Random(1);
+            int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int[] expected = {9, 3, 7, 5, 4, 1, 10, 8, 2, 6};
+            arr = FisherYatesShuffle.Shuffle(arr, rnd);
+
+            Assert.Equal(expected, arr);
+        }
+
+        [Fact]
+        public void ShuffleEnumerable_ShufflesElements()
+        {
+            var rnd = new Random(1);
+            IEnumerable<int> arr = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            IEnumerable<int> expected = new[] {9, 3, 7, 5, 4, 1, 10, 8, 2, 6};
+            arr = FisherYatesShuffle.Shuffle(arr.AsEnumerable(), rnd);
+
+            Assert.Equal(expected, arr);
+        }
+    }
+}
