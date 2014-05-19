a47a604d9f5f6cfdb2544039fc9e4b23528b7d0d
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-05-19 11:16:46 +0100

    Added QuickSort (out-of-place)

---
+++ src/Algorithms/QuickSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class QuickSort
+    {
+        public static List<T> Sort<T>(List<T> source, IComparer<T> cmp = null)
+        {
+            cmp = cmp ?? Comparer<T>.Default;
+
+            if (source.Count <= 1)
+                return source;
+
+            int pivotIndex = source.Count >> 1;
+            T pivot = source[pivotIndex];
+
+            var less = new List<T>();
+            var greater = new List<T>();
+
+            IEnumerable<T> withoutPivot = source.Take(pivotIndex)
+                                                .Concat(source.Skip(pivotIndex + 1));
+
+            foreach (var elem in withoutPivot)
+            {
+                if (cmp.Compare(elem, pivot) > 0)
+                    greater.Add(elem);
+                else
+                    less.Add(elem);
+            }
+
+            var res = Sort(less);
+            res.Add(pivot);
+            res.AddRange(Sort(greater));
+
+            return res;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/QuickSortTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class QuickSortTests
+    {
+        [Fact]
+        public void Sort_SortsElements()
+        {
+            var nums = new List<int> {5, 3, 9, 7, 6, 1, 8, 4, 2};
+            int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            IEnumerable<int> sorted = QuickSort.Sort(nums);
+
+            Assert.Equal(expected, sorted);
+        }
+    }
+}
