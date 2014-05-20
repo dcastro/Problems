4b8de9a13e403771b529391641dcce382ff83a9d
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-05-20 14:06:51 +0100

    Added LinkedList "midpoint" algorithm

---
+++ src/Algorithms/LinkedList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        public Node<T> Head { get; set; }
+
+        public void Add(Node<T> node)
+        {
+            node.Next = Head;
+            Head = node;
+        }
+
+        //O(1) space
+        //O(n) processing time
+        public Node<T> Midpoint()
+        {
+            Node<T> mid = Head;
+            Node<T> last = mid;
+
+            bool isEven = false;
+            while (last != null)
+            {
+                last = last.Next;
+
+                if(isEven)
+                    mid = mid.Next;
+
+                isEven = !isEven;
+            }
+
+            return mid;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> last = Head;
+            while (last != null)
+            {
+                yield return last.Data;
+                last = last.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class Node<T>
+    {
+        public Node(T value)
+        {
+            Data = value;
+        }
+
+        public T Data { get; set; }
+
+        public Node<T> Next { get; set; }
+    }
+
+}
---
+++ tests/Algorithms.Tests/LinkedListMidpointTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class LinkedListMidpointTest
+    {
+        [Fact]
+        public void Midpoint_When_CountIsOdd()
+        {
+            var list = new LinkedList<int>
+                {
+                    new Node<int>(1),
+                    new Node<int>(2),
+                    new Node<int>(3),
+                    new Node<int>(4),
+                    new Node<int>(5)
+                };
+
+            Assert.Equal(3, list.Midpoint().Data);
+        }
+
+        [Fact]
+        public void Midpoint_When_CountIsEven()
+        {
+            var list = new LinkedList<int>
+                {
+                    new Node<int>(1),
+                    new Node<int>(2),
+                    new Node<int>(3),
+                    new Node<int>(4)
+                };
+
+            Assert.Equal(2, list.Midpoint().Data);
+        }
+
+        [Fact]
+        public void MidPoint_When_CountIsZero()
+        {
+            var list = new LinkedList<int>
+                {
+                };
+
+            Assert.Null(list.Midpoint());
+        }
+
+        [Fact]
+        public void MidPoint_When_CountIsOne()
+        {
+            var list = new LinkedList<int>
+            {
+                new Node<int>(1)
+            };
+
+            Assert.Equal(1, list.Midpoint().Data);
+        }
+    }
+}
