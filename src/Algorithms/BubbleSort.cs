8169e0ff42361614a22c2463998153b16ee16968
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-05-20 19:47:33 +0100

    Added BubbleSort

---
+++ src/Algorithms/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Extensions;
+
+namespace Algorithms
+{
+    public static class BubbleSort
+    {
+        public static void SortInPlace<T>(IList<T> source, IComparer<T> cmp = null)
+        {
+            cmp = cmp ?? Comparer<T>.Default;
+
+            bool swapped;
+
+            do
+            {
+                swapped = false;
+                for (int i = 0; i <= source.Count - 2; i++)
+                {
+                    if (cmp.Compare(source[i], source[i + 1]) > 0)
+                    {
+                        source.Swap(i, i + 1);
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/BubbleSortTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class BubbleSortTests
+    {
+        [Fact]
+        public void Sort()
+        {
+            int[] nums = { 5, 3, 9, 7, 6, 1, 8, 4, 2 };
+            int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            BubbleSort.SortInPlace(nums);
+
+            Assert.Equal(expected, nums);
+        }
+    }
+}
