144c8a1d77c696ed53ea16702ad15a8734fbb484
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-05-22 08:49:42 +0100

    Find the K-th lowest number from 2 sorted arrays

---
+++ src/Algorithms/KthLowestNumberFrom2SortedArrays.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class KthLowestNumberFrom2SortedArrays
+    {
+        public static int Calc(int[] a, int[] b, int k)
+        {
+            int leftA = 0, leftB = 0;
+            int rightA = Math.Min(a.Length - 1, k - 1);
+            int rightB = Math.Min(b.Length - 1, k - 1);
+
+            int midA = rightA/2;
+            int midB = rightB/2;
+
+            while (leftA <= rightA && leftB <= rightB)
+            {
+                int total = midA + midB + 2;
+
+                if (a[midA] < b[midB])
+                {
+                    if (total > k)
+                    {
+                        rightB = midB - 1;
+                        midB = ((rightB - leftB)/2) + leftB;
+                    }
+                    else
+                    {
+                        leftA = midA + 1;
+                        midA = ((rightA - leftA)/2) + leftA;
+                    }
+                }
+                else //if a[midA] >= b[midB]
+                {
+                    if (total > k)
+                    {
+                        rightA = midA - 1;
+                        midA = ((rightA - leftA)/2) + leftA;
+                    }
+                    else
+                    {
+                        leftB = midB + 1;
+                        midB = ((rightB - leftB)/2) + leftB;
+                    }
+                }
+            }
+
+            if (leftA <= rightA)
+                return a[k - leftB - 1];
+            return b[k - leftA - 1];
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/KthLowestNumberFrom2SortedArraysTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class KthLowestNumberFrom2SortedArraysTests
+    {
+        [Fact]
+        public void Test()
+        {
+            int[] a = {1, 4, 5, 6, 7, 8};
+            int[] b = {2, 3, 10, 11, 12, 13};
+
+            int res = KthLowestNumberFrom2SortedArrays.Calc(a, b, 5);
+
+            Console.WriteLine("!!! " + res);
+        }
+    }
+}
