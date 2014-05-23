400c77949d1bcebeab754e1b81e31a98da0e6509
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-05-23 12:25:10 +0100

    The Next Palindrome problem from SPOJ
    http://www.spoj.com/problems/PALIN/

---
+++ src/Algorithms/Palindrome.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class Palindrome
+    {
+        /**
+         * http://www.spoj.com/problems/PALIN/
+         * 
+         * The Next Palindrome
+         * 
+         * A positive integer is called a palindrome if its representation in the decimal system is the same when read from left to right and from right to left.
+         * For a given positive integer K of not more than 1000000 digits, write the value of the smallest palindrome larger than K to output.
+         * Numbers are always displayed without leading zeros.
+         * 
+         * Input:
+         * The first line contains integer t, the number of test cases. Integers K are given in the next t lines.
+         * 
+         * Output:
+         * For each K, output the smallest palindrome larger than K.
+         * 
+         * Example:
+         * Input:
+         * 2
+         * 808
+         * 2133
+         * Output:
+         * 818
+         * 2222
+         */
+
+        public static int Next(int k)
+        {
+            if (k.IsPalindrome())
+                k++;
+
+            return Next(k.ToString(CultureInfo.InvariantCulture)
+                         .Select(ch => (int) Char.GetNumericValue(ch))
+                         .ToArray());
+        }
+
+        private static int Next(int[] k)
+        {
+            /**
+             * The basic idea behind the algorithm is to mirror the left part of the number.
+             * I.e., to transpose the most significant digits (MSDs) on top of the least significant digits in reverse order.
+             * E.g.,
+             * Input:   [4321] => [43][21]
+             * Mirror:  [43][34] => [4334] 
+             * 
+             * If the mirrored number is less than "k" (e.g., k = 1234, mirror(k) = 1221, which is less than "k"),
+             * then we increment the number formed by the most significant digits by 1 before mirroring the number.
+             * E.g.,
+             * Input:           [12][34]
+             * Increment MSDs:  [13][34]
+             * Mirror:          [13][31] => [1331]
+             * 
+             */
+
+            if (!IsMirrorValid(k))
+                IncrementMSDs(k);
+
+            MirrorMSDs(k);
+
+            return DigitsToNumber(k);
+        }
+
+        private static int DigitsToNumber(int[] digits)
+        {
+            int number = 0;
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                int digit = digits[digits.Length - 1 - i];
+
+                number += (int) (digit*Math.Pow(10, i));
+            }
+
+            return number;
+        }
+
+        private static bool IsMirrorValid(int[] k)
+        {
+            int left = (k.Length - 1)/2;
+            int right = k.Length - 1 - left;
+
+            while (left >= 0)
+            {
+                if (k[left] > k[right])
+                    return true;
+                if (k[left] < k[right])
+                    return false;
+
+                left--;
+                right++;
+            }
+
+            return true;
+        }
+
+        private static void IncrementMSDs(int[] k)
+        {
+            int current = (k.Length - 1)/2;
+            do
+            {
+                k[current]++;
+
+                if (k[current] == 10)
+                    k[current] = 0;
+
+            } while (k[current--] == 0);
+        }
+
+        private static void MirrorMSDs(int[] k)
+        {
+            int left = (k.Length - 1)/2;
+            int right = k.Length - 1 - left;
+
+            while (left >= 0)
+            {
+                k[right] = k[left];
+                left--;
+                right++;
+            }
+        }
+
+        public static bool IsPalindrome<T>(this T[] source)
+        {
+            int left = 0, right = source.Length - 1;
+
+            while (left < right)
+                if (!source[left++].Equals(source[right--]))
+                    return false;
+            return true;
+        }
+
+
+        public static bool IsPalindrome(this int source)
+        {
+            return source.ToString(CultureInfo.InvariantCulture)
+                         .ToCharArray()
+                         .IsPalindrome();
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/PalindromeTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Algorithms.Tests
+{
+    public class PalindromeTests
+    {
+        [Theory]
+        [InlineData(2133, 2222)]
+        [InlineData(2113, 2222)]
+        [InlineData(2101, 2112)]
+        [InlineData(808, 818)]
+        [InlineData(199, 202)]
+        [InlineData(103, 111)]
+        [InlineData(1, 2)]
+        public void NextPalindrome(int k, int expectedPalindrome)
+        {
+            Assert.Equal(expectedPalindrome, Palindrome.Next(k));
+        }
+
+        [Theory]
+        [PropertyData("NextPalindromeData")]
+        public void NextPalindrome2(int k)
+        {
+            int pal = Palindrome.Next(k);
+
+            Assert.True(pal.IsPalindrome());
+
+            //if there's at least one number between "k" and "pal", those numbers must NOT be palindromes
+            if (pal - k > 1)
+            {
+                IEnumerable<int> nonPalindromes = Enumerable.Range(k + 1, pal - k - 1);
+                foreach (var nonPalindrome in nonPalindromes)
+                    Assert.False(nonPalindrome.IsPalindrome());
+            }
+        }
+
+        public static IEnumerable<object[]> NextPalindromeData
+        {
+            get { return Enumerable.Range(1, 1000).Select(i => new object[] {i}); }
+        }
+
+        [Theory]
+        [InlineData("1221", true)]
+        [InlineData("12321", true)]
+        [InlineData("123a1", false)]
+        [InlineData("12a1", false)]
+        public void IsPalindrome(string str, bool isPalindrome)
+        {
+            Assert.Equal(isPalindrome, str.ToCharArray().IsPalindrome());
+        }
+    }
+}
