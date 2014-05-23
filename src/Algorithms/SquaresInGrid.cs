f2fa6ff554f8af1fba078cfba4e722594a31b21e
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-05-23 13:52:46 +0100

    The Feynman problem from SPOJ
    http://www.spoj.com/problems/SAMER08F/

---
+++ src/Algorithms/SquaresInGrid.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class SquaresInGrid
+    {
+        /**
+         * http://www.spoj.com/problems/SAMER08F/
+         * 
+         * Feynman
+         * 
+         * Richard Phillips Feynman was a well known American physicist and a recipient of the Nobel Prize in Physics.
+         * He worked in theoretical physics and also pioneered the field of quantum computing.
+         * He visited South America for ten months, giving lectures and enjoying life in the tropics.
+         * He is also known for his books "Surely You're Joking, Mr. Feynman!" and "What Do You Care What Other People Think?", which include some of his adventures below the equator.
+         * 
+         * His life-long addiction was solving and making puzzles, locks, and cyphers.
+         * Recently, an old farmer in South America, who was a host to the young physicist in 1949, found some papers and notes that is believed to have belonged to Feynman.
+         * Among notes about mesons and electromagnetism, there was a napkin where he wrote a simple puzzle: "how many different squares are there in a grid of N ×N squares?".
+         * In the same napkin there was a drawing which is reproduced below, showing that, for N=2, the answer is 5.
+         * 
+         * http://www.spoj.com/content/disatoba:feynman.gif
+         * 
+         */
+
+        public static int Solve(int n)
+        {
+            //f(n) = Σ (1>= i >= n) (i^2)
+            int squares = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                squares += i*i;
+            }
+
+            return squares;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/SquaresInGridTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Algorithms.Tests
+{
+    public class SquaresInGridTests
+    {
+        [Theory]
+        [InlineData(2, 5)]
+        [InlineData(1, 1)]
+        [InlineData(8, 204)]
+        public void Test(int n, int expectedSquares)
+        {
+            Assert.Equal(expectedSquares, SquaresInGrid.Solve(n));
+        }
+    }
+}
