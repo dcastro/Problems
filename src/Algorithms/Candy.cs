2dd9fe87e1d64a9a58720bc67bbce2b7f8979017
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-05-23 18:06:02 +0100

    Candy moving problem from SPOJ
    http://www.spoj.com/problems/CANDY/

---
+++ src/Algorithms/Candy.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Extensions;
+
+namespace Algorithms
+{
+    public static class Candy
+    {
+        /**
+         * http://www.spoj.com/problems/CANDY/
+         * 
+         * Jennifer is a teacher in the first year of a primary school.
+         * She has gone for a trip with her class today.
+         * She has taken a packet of candies for each child. Unfortunatelly, the sizes of the packets are not the same.
+         * 
+         * Jennifer is afraid that each child will want to have the biggest packet of candies and this will lead to quarrels
+         * or even fights among children. She wants to avoid this.
+         * Therefore, she has decided to open all the packets, count the candies in each packet and move some candies from bigger
+         * packets to smaller ones so that each packet will contain the same number of candies.
+         * The question is how many candies she has to move.
+         */
+
+        //O(n log n) time
+        public static int Solve(params int[] bags)
+        {
+            int sum = bags.Sum();
+
+            //check if the candies can be evenly distributed among the bags
+            if (sum%bags.Length != 0)
+                return -1;
+
+            //sort the bags
+            QuickSort.SortInPlace(bags);
+            int moveCount = 0;
+
+            while (bags.Last() != bags.First())
+            {
+                //take a candy from the bag with the most candy
+                //and put it in the bag with the least candy
+                bags[bags.Length - 1]--;
+                bags[0]++;
+                moveCount++;
+
+                //reorder these two bags
+                SortFirstElement(bags);
+                SortLastElement(bags);
+            }
+
+            return moveCount;
+        }
+
+        private static void SortFirstElement(int[] buffer)
+        {
+            int index = 0;
+            while (index < buffer.Length - 1 && buffer[index] > buffer[index + 1])
+            {
+                buffer.Swap(index, index + 1);
+                index++;
+            }
+        }
+
+        private static void SortLastElement(int[] buffer)
+        {
+            int index = buffer.Length - 1;
+            while (index > 0 && buffer[index - 1] > buffer[index])
+            {
+                buffer.Swap(index, index - 1);
+                index--;
+            }
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/CandyTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Algorithms.Tests
+{
+    public class CandyTests
+    {
+        [Theory]
+        [InlineData(4, new[] {1,1,1,1,6})]
+        [InlineData(-1, new[] {2,3})]
+        public void Test(int expected, int[] bags)
+        {
+            int moves = Candy.Solve(bags);
+
+            Console.WriteLine(moves);
+            Assert.Equal(expected, moves);
+        }
+    }
+}
