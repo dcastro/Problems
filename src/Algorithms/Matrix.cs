43ee130692a52c44ab5fcf142d127c7975382654
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-05-24 21:38:57 +0100

    Add matrix product

---
+++ src/Algorithms/Matrix.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Matrix : IEnumerable<int[]>
+    {
+        private readonly int[][] _rows;
+        private int _rowsFilled;
+
+        public Matrix(int rows, int columns)
+        {
+            _rows = new int[rows][];
+
+            for (int i = 0; i < _rows.Length; i++)
+                _rows[i] = new int[columns];
+        }
+
+        public static Matrix operator *(Matrix m1, Matrix m2)
+        {
+            if(m1.ColumnCount != m2.RowCount)
+                throw new ArgumentException("m1's columns must equal m2's rows.");
+
+            var m3 = new Matrix(m1.RowCount, m2.ColumnCount);
+            
+            for (int i = 0; i < m1.RowCount; i++)
+                for (int j = 0; j < m1.ColumnCount; j++)
+                    for (int k = 0; k < m2.ColumnCount; k++)
+                        m3[i][k] += m1[i][j] * m2[j][k];
+
+            return m3;
+        }
+
+
+        public int[] this[int index]
+        {
+            get { return _rows[index]; }
+        }
+
+        public int RowCount
+        {
+            get { return _rows.Length; }
+        }
+
+        public int ColumnCount
+        {
+            get { return _rows[0].Length; }
+        }
+
+        public void Add(params int[] row)
+        {
+            _rows[_rowsFilled++] = row;
+        }
+
+        public IEnumerator<int[]> GetEnumerator()
+        {
+            return _rows.AsEnumerable().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/MatrixTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class MatrixTests
+    {
+
+        [Fact]
+        public void Product()
+        {
+            var m1 = new Matrix(2, 3)
+                {
+                    {1, 2, 3},
+                    {4, 5, 6}
+                };
+
+            var m2 = new Matrix(3, 2)
+                {
+                    {7, 8},
+                    {9, 10},
+                    {11, 12}
+                };
+
+            var m3 = m1*m2;
+
+            Assert.Equal(2, m3.RowCount);
+            Assert.Equal(2, m3.ColumnCount);
+
+            Assert.Equal(new[] {58, 64}, m3[0]);
+            Assert.Equal(new[] {139, 154}, m3[1]);
+
+        }
+    }
+}
