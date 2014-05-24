00cf56ad4307c9c3dff24a69d3b178935f20a6ab
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-05-24 15:23:46 +0100

    Number Steps problem from SPOJ
    http://www.spoj.com/problems/NSTEPS/

---
+++ src/Algorithms/NumberSteps.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Algorithms.Utils;
+
+namespace Algorithms
+{
+    public static class NumberSteps
+    {
+        /**
+         * http://www.spoj.com/problems/NSTEPS/
+         * 
+         * 1112. Number Steps
+         * 
+         * Starting from point (0,0) on a plane, we have written all non-negative integers 0, 1, 2,... as shown in the figure.
+         * For example, 1, 2, and 3 has been written at points (1,1), (2,0), and (3, 1) respectively and this pattern has continued.
+         * 
+         * http://www.spoj.com/content/steinersp:nsteps.gif
+         * 
+         * You are to write a program that reads the coordinates of a point (x, y),
+         * and writes the number (if any) that has been written at that point.
+         * (x, y) coordinates in the input are in the range 0...10000.
+         * 
+         */
+
+        public static object Solve(int x, int y)
+        {
+            if (y != x && y != x - 2)
+                return "No Number";
+
+            if (y%2 == 0)
+                return x + y;
+            return x + y - 1;
+        }
+
+        public static void Run()
+        {
+            int count = Input.NextInt();
+
+            while (count-- > 0)
+            {
+                string pair = Input.NextString();
+                var coords = pair.Split(' ');
+                Console.WriteLine(
+                    Solve(
+                        int.Parse(coords[0]),
+                        int.Parse(coords[1])));
+            }
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/NumberStepsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Algorithms.Tests
+{
+    public class NumberStepsTests
+    {
+        [Theory]
+        [InlineData(4, 2, 6)]
+        [InlineData(6, 6, 12)]
+        [InlineData(3, 4, "No Number")]
+        public void Test(int x, int y, object expected)
+        {
+            Assert.Equal(expected, NumberSteps.Solve(x, y));
+        }
+    }
+}
